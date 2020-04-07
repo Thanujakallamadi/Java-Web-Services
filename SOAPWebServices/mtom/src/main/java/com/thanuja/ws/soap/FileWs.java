54c63624983932cf8ff1f172583c4eb140744044
Thanuja Kallamadi <kallamadithanuja@gmail.com>
2020-04-06 22:58:13 -0700

    mtom ws

---
+++ SOAPWebServices/mtom/src/main/java/com/thanuja/ws/soap/FileWs.java
@@ -0,0 +1,13 @@
+package com.thanuja.ws.soap;
+
+import javax.activation.DataHandler;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+public interface FileWs {
+
+	void upload(@WebParam(name="file") DataHandler attachment);
+	DataHandler download();
+	
+}
